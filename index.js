a6d3ab0c7a61abee8a064aa2f2deff8ea7b1c868
floresmatias <floresmatias0@gmail.com>
2021-11-13 22:28:08 -0300

    ADD bulkcreate

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+const { conn, Category } = require('./src/db.js');
+const { categories } = require('./src/helpers');
 
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
+  Category.bulkCreate(categories)
   server.listen(process.env.PORT || 3001, () => {
     console.log('listening at 3001'); // eslint-disable-line no-console
   });
---
+++ src/helpers/index.js
@@ -0,0 +1,25 @@
+const categories = [{
+    name: 'documentacion y mantenimiento',
+    icon: 'archivo'
+},{
+    name: 'ruedas, frenos, suspensión y dirección',
+    icon: 'rueda'
+},{
+    name: 'carroceria',
+    icon: 'motor'
+},{
+    name: 'motor',
+    icon: 'motor'
+},{
+    name: 'alumbrado',
+    icon: 'lampara'
+},{
+    name: 'volante y tablero',
+    icon: 'volante'
+},{
+    name: 'interior y asientos',
+    icon: 'asiento'
+},{
+    name: 'interior general',
+    icon: 'motor'
+}]
