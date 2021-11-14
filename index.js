62b840fd7bba66a061e454da64dee2c501b838b3
floresmatias <floresmatias0@gmail.com>
2021-11-13 22:53:51 -0300

    ADD bulkcreate 2

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const server = require('./src/app.js');
-const { conn, Category } = require('./src/db.js');
-const { categories } = require('./src/helpers');
+const { conn, Category, Property } = require('./src/db.js');
+const {
+  categories,
+  properties
+} = require('./src/helpers');
 
 conn.sync({ force: true }).then(() => {
   Category.bulkCreate(categories)
+  Property.bulkCreate(properties)
   server.listen(process.env.PORT || 3001, () => {
     console.log('listening at 3001'); // eslint-disable-line no-console
   });
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -23,3 +23,175 @@ const categories = [{
     name: 'interior general',
     icon: 'motor'
 }]
+
+const properties = [{
+    name: 'Cédula verde, azul, título del automotor',
+    categoryId: 1
+},{
+    name: 'VTV vigente',
+    categoryId: 1
+},{
+    name: 'Segundo juego de llaves',
+    categoryId: 1
+},{
+    name: 'Informe de dominio histórico',
+    categoryId: 1
+},{
+    name: 'Escaneo computarizado',
+    categoryId: 1
+},{
+    name: 'Verificacion policial vigente',
+    categoryId: 1
+},{
+    name: 'Manual de usuario con servicios registrados',
+    categoryId: 1
+},{
+    name: 'Funcionamiento del control remoto',
+    categoryId: 1
+},{
+    name: 'Libre de deudas financieras, multas y patentes',
+    categoryId: 1
+},{
+    name: 'Modelo identico de neumáticos',
+    categoryId: 2
+},{
+    name: 'Desgaste del neumático',
+    categoryId: 2
+},{
+    name: 'Presión adecuada del neumático',
+    categoryId: 2
+},{
+    name: 'Llanta sin daños',
+    categoryId: 2
+},{
+    name: 'Tuercas de seguridad',
+    categoryId: 2
+},{
+    name: 'Espesor de la pastilla de freno',
+    categoryId: 2
+},{
+    name: 'Bielas',
+    categoryId: 2
+},{
+    name: 'Rótulas',
+    categoryId: 2
+},{
+    name: 'Tipo de neumático correcto',
+    categoryId: 2
+},{
+    name: 'Desgaste homogéneo de neumáticos',
+    categoryId: 2
+},{
+    name: 'Neumático sin daños',
+    categoryId: 2
+},{
+    name: 'Tornillos y llave de llanta',
+    categoryId: 2
+},{
+    name: 'Espesor del disco de freno',
+    categoryId: 2
+},{
+    name: 'Brazo de suspensión',
+    categoryId: 2
+},{
+    name: 'Manguetas',
+    categoryId: 2
+},{
+    name: 'Extremos de dirección',
+    categoryId: 2
+},{
+    name: 'Bajos del paragolpe delantero',
+    categoryId: 3
+},{
+    name: 'Capó',
+    categoryId: 3
+},{
+    name: 'Guardabarro delantero derecho',
+    categoryId: 3
+},{
+    name: 'Espejo retrovisor izquierdo',
+    categoryId: 3
+},{
+    name: 'Panel exterior (puerta delantera izquierda)',
+    categoryId: 3
+},{
+    name: 'Bisagra (puerta delantera izquierda)',
+    categoryId: 3
+},{
+    name: 'Manija (puerta delantera derecha)',
+    categoryId: 3
+},{
+    name: 'Parabrisas delantero',
+    categoryId: 3
+},{
+    name: 'Paragolpe delantero',
+    categoryId: 3
+},{
+    name: 'Guardabarro delantero izquierdo',
+    categoryId: 3
+},{
+    name: 'Parabrisas',
+    categoryId: 3
+},{
+    name: 'Manija (puerta delantera izquierda)',
+    categoryId: 3
+},{
+    name: 'Panel exterior (puerta delantera derecha)',
+    categoryId: 3
+},{
+    name: 'Bisagra (puerta delantera derecha)',
+    categoryId: 3
+},{
+    name: 'Talonera izquierda',
+    categoryId: 3
+},{
+    name: 'Estado aparente del motor',
+    categoryId: 4
+},{
+    name: 'Estado del aceite',
+    categoryId: 4
+},{
+    name: 'Nivel del liquido de frenos',
+    categoryId: 4
+},{
+    name: 'Nivel del liquido de direccion hidráulica',
+    categoryId: 4
+},{
+    name: 'Cadena o correa de distribución',
+    categoryId: 4
+},{
+    name: 'Ruidos y vibraciones anómalas: motor frio',
+    categoryId: 4
+},{
+    name: 'Circuito de transmision',
+    categoryId: 4
+},{
+    name: 'Circuito de frenos',
+    categoryId: 4
+},{
+    name: 'Nivel de aceite',
+    categoryId: 4
+},{
+    name: 'Nivel y estado del liquido refrigerante',
+    categoryId: 4
+},{
+    name: 'Estado del liquido de frenos',
+    categoryId: 4
+},{
+    name: 'Nivel del liquido de limpia parabrisas',
+    categoryId: 4
+},{
+    name: 'Correa de elementos auxiliares (Alternador/Ventilador/AA)',
+    categoryId: 4
+},{
+    name: 'Circuito de lubricacion',
+    categoryId: 4
+},{
+    name: 'Circuito del refrigerante',
+    categoryId: 4
+},{
+    name: 'Circuito del combustible',
+    categoryId: 4
+}]
+
+module.exports = {categories,properties};
