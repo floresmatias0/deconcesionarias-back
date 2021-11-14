e165769978ed0cbf2d9e89295d6ee43c8ac19992
floresmatias <floresmatias0@gmail.com>
2021-11-13 22:16:29 -0300

    ADD PORT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
+  server.listen(process.env.PORT || 3001, () => {
     console.log('listening at 3001'); // eslint-disable-line no-console
   });
 });
